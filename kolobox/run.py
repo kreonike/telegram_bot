3e554b2b5f4c4f8f7f7036f8f7584e7ba33fb06e
kreonike <kreonike@gmail.com>
2024-05-31 11:30:33 +0300

    home02

--- kolobox/run.py
+++ kolobox/run.py
@@ -16,168 +16,64 @@ bot = Bot(token=token)
 dp = Dispatcher()
 
 
-def komedy():
+def comedy():
     url = "https://api.kinopoisk.dev/v1.4/movie?rating.imdb=8-10&year=2023&year=2024&genres.name=комедия"
-    #headers = {"accept": "application/json"}
+    # headers = {"accept": "application/json"}
     headers = {"X-API-KEY": kinopoisk}
     response = requests.get(url, headers=headers)
     data = response.json()
     return data
+
 
 def drama():
     url = "https://api.kinopoisk.dev/v1.4/movie?rating.imdb=8-10&year=2023&year=2024&genres.name=драма"
-    #headers = {"accept": "application/json"}
+    # headers = {"accept": "application/json"}
     headers = {"X-API-KEY": kinopoisk}
     response = requests.get(url, headers=headers)
     data = response.json()
     return data
 
+
 def fantastik():
     url = "https://api.kinopoisk.dev/v1.4/movie?rating.imdb=8-10&year=2023&year=2024&genres.name=фантастика"
-    #headers = {"accept": "application/json"}
+    # headers = {"accept": "application/json"}
     headers = {"X-API-KEY": kinopoisk}
     response = requests.get(url, headers=headers)
     data = response.json()
     return data
+
 
 @dp.message(CommandStart())
 async def cmd_start(message: Message):
     await message.reply('Привет, фильмы какого жанда Вас интересуют ?', reply_markup=kb.main)
 
 
-
-
-
 @dp.message(F.text == "Комедия")
 async def with_puree(message: Message):
     await message.reply('Отличный выбор')
-    data = komedy()
-    k_data = []
-    string = ''
-    for i in data['docs']:
-        name = i['name']
-        alternativeName = i['alternativeName']
-        description = i['description']
-        type = i['type']
-        year = i['year']
-        rating = i['rating']['imdb']
-        genres = i['genres']
-
-        k_data.append('Название Фильма: ')
-        k_data.append(name)
-        k_data.append('\n')
-        k_data.append('Оригинальное название: ')
-        k_data.append(alternativeName)
-        k_data.append('\n')
-        k_data.append('Описание: ')
-        k_data.append(description)
-        k_data.append('\n')
-        k_data.append('Тип: ')
-        k_data.append(type)
-        k_data.append('\n')
-        k_data.append('Год выпуска: ')
-        k_data.append(year)
-        k_data.append('\n')
-        k_data.append('Рейтинг: ')
-        k_data.append(rating)
-        k_data.append('\n')
-        k_data.append('Жанр: ')
-        k_data.append(genres)
-        k_data.append('\n')
-        k_data.append('\n')
-
-    for t in k_data:
-        string += str(t)
-
-    print(string)
-    await message.reply(string)
-
-    # print(f' Название фильма:', name, '\n'
-        #       f' Оригинальное название:,', alternativeName, '\n'
-        #       f' Описание:,', description, '\n')
-
-
-
-    # print(k_data)
-    # print(string)
-    # await message.reply(string)
-        # print(f' Название фильма:', name, "\n"
-        #                                   f' Оригинальное название:', alternativeName, "\n"
-        #                                                                                f' Год:', year, "\n"
-        #                                                                                                f' Описание:',
-        #       description, "\n"
-        #                    f' Тип:', type, "\n"
-        #                                    f' Рейтинг:', rating, "\n"
-        #                                                          f' Жанр:', genres), "\n"
-        # print("\n")
+    total_comedy = comedy()
+    total_data = data_result(total_comedy)
+    await message.reply(total_data)
 
 
 @dp.message(F.text == "Драма")
 async def without_puree(message: Message):
     await message.reply('Отличный выбор')
-    data = drama()
-    k_data = []
-    string = ''
-    for i in data['docs']:
-        name = i['name']
-        alternativeName = i['alternativeName']
-        description = i['description']
-        type = i['type']
-        year = i['year']
-        rating = i['rating']['imdb']
-        genres = i['genres']
-
-        k_data.append('Название Фильма: ')
-        k_data.append(name)
-        k_data.append('\n')
-        k_data.append('Оригинальное название: ')
-        k_data.append(alternativeName)
-        k_data.append('\n')
-        k_data.append('Описание: ')
-        k_data.append(description)
-        k_data.append('\n')
-        k_data.append('Тип: ')
-        k_data.append(type)
-        k_data.append('\n')
-        k_data.append('Год выпуска: ')
-        k_data.append(year)
-        k_data.append('\n')
-        k_data.append('Рейтинг: ')
-        k_data.append(rating)
-        k_data.append('\n')
-        k_data.append('Жанр: ')
-        k_data.append(genres)
-        k_data.append('\n')
-        k_data.append('\n')
-
-    for t in k_data:
-        string += str(t)
-
-    print(string)
-    await message.reply(string)
-    # for i in data['docs']:
-    #     name = i['name']
-    #     alternativeName = i['alternativeName']
-    #     description = i['description']
-    #     type = i['type']
-    #     year = i['year']
-    #     rating = i['rating']['imdb']
-    #     genres = i['genres']
-    #     print(f' Название фильма:', name, "\n"
-    #                                       f' Оригинальное название:', alternativeName, "\n"
-    #                                                                                    f' Год:', year, "\n"
-    #                                                                                                    f' Описание:',
-    #           description, "\n"
-    #                        f' Тип:', type, "\n"
-    #                                        f' Рейтинг:', rating, "\n"
-    #                                                              f' Жанр:', genres), "\n"
-    #     print("\n")
+    await message.reply('Отличный выбор')
+    total_drama = drama()
+    total_data = data_result(total_drama)
+    await message.reply(total_data)
 
 
 @dp.message(F.text == "Фантастика")
 async def without_puree(message: Message):
     await message.reply('Отличный выбор')
-    data = fantastik()
+    total_fantastik = fantastik()
+    total_data = data_result(total_fantastik)
+    await message.reply(total_data)
+
+
+def data_result(data):
     k_data = []
     string = ''
     for i in data['docs']:
@@ -214,13 +110,7 @@ async def without_puree(message: Message):
 
     for t in k_data:
         string += str(t)
-
-    print(string)
-    await message.reply(string)
-
-def data_result(data):
-    return
-
+    return string
 
 
 async def main():
@@ -234,4 +124,3 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print('Exit')
     asyncio.run(main())
-
